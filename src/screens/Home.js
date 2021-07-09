f17281dc7fe4a4a481232cf7d0d327b28a1b5aad
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-07-09 18:03:26 -0300

    Create and organize the project files

---
+++ src/screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
