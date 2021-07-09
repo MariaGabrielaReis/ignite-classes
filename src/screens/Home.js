a46d2c37c271def653798480a3a3b84e03bc1644
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-07-09 18:58:10 -0300

    Create element of list and states to list skills (with useState)

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   StyleSheet,
@@ -9,6 +9,13 @@ import {
 } from 'react-native';
 
 export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [skills, setSkills] = useState([]);
+
+  function handleAddNewSkill() {
+    setSkills((oldSkills) => [...oldSkills, newSkill]);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Welcome, Maby</Text>
@@ -17,15 +24,26 @@ export function Home() {
         style={styles.input}
         placeholder={'New skill'}
         placeholderTextColor={'#666'}
+        onChangeText={setNewSkill}
       />
 
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+        onPress={handleAddNewSkill}
+      >
         <Text style={styles.buttonText}>Add</Text>
       </TouchableOpacity>
 
-      <Text style={[styles.title, { marginTop: 40, alignSelf: 'flex-start' }]}>
+      <Text style={[styles.title, { marginTop: 48, alignSelf: 'flex-start' }]}>
         My Skills
       </Text>
+
+      {skills.map((skill) => (
+        <TouchableOpacity key={skill} style={styles.skill}>
+          <Text style={styles.skillText}>{skill}</Text>
+        </TouchableOpacity>
+      ))}
     </SafeAreaView>
   );
 }
@@ -47,7 +65,7 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: '#272B34',
     color: '#FFFFFF',
-    fontSize: 18,
+    fontSize: 24,
     padding: Platform === 'ios' ? 20 : 15,
     borderRadius: 8,
   },
@@ -56,9 +74,20 @@ const styles = StyleSheet.create({
     padding: 15,
     alignItems: 'center',
     borderRadius: 8,
-    marginTop: 20,
+    marginTop: 24,
   },
   buttonText: {
+    color: '#FFFFFF',
+    fontSize: 24,
+  },
+  skill: {
+    backgroundColor: '#272B34',
+    padding: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  skillText: {
     color: '#FFFFFF',
     fontSize: 24,
   },
