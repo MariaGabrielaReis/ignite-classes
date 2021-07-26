5cfdfe77e7da1d559cc1621ee5bf05c98f38718d
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-07-26 16:30:18 -0300

    Create register screen header

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 export default function App() {
   const [fontsLoadead] = useFonts({
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Header, Title } from './styles';
+
+export function Register() {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+    </Container>
+  );
+}
